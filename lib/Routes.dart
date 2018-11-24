51d1ee056a2bd13b95f420c994d45dea1e444885
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2018-11-24 01:29:50 +0100

    Update Routes.dart
    Fix #6 thanks to @IAmSarthakVerma

--- lib/Routes.dart
+++ lib/Routes.dart
@@ -5,10 +5,10 @@ import 'package:planets/ui/planet_detail/PlanetDetailPage.dart';
 class Routes {
   static final Router _router = new Router();
 
-
   static var planetDetailHandler = new Handler(
     handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new PlanetDetailPage(params["id"]);
+      debugPrint(params["id"].toString().substring(1, params["id"].toString().length-1));
+      return new PlanetDetailPage(params["id"].toString().substring(1, params["id"].toString().length-1));
     });
 
   static void initRoutes() {
@@ -18,5 +18,4 @@ class Routes {
   static void navigateTo(context, String route, {TransitionType transition}) {
     _router.navigateTo(context, route, transition: transition);
   }
-
 }
